53a5ab5c4158710db05d37c97ad4544be377e368
Aaron <caper2711@hotmail.com>
2024-11-15 20:56:25 -0600

    Hate this crap

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -2,41 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingPlatform : MonoBehaviour {
+public class MovingPlatform : MonoBehaviour
+{
     public Transform target;
     public float speed = 10;
     private Vector3 posA, posB;
+    public Transform playerOriginalParent;
 
-    void Start() {
-        if (target != null) {
+    void Start()
+    {
+        if (target != null)
+        {
             target.parent = null;
             posA = transform.position;
             posB = target.position;
         }
     }
 
-    void FixedUpdate() {
-        if (target != null) {
+    void FixedUpdate()
+    {
+        if (target != null)
+        {
             float fixedSpeed = speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, target.position, fixedSpeed);
         }
-        if (transform.position == target.position) {
+        if (transform.position == target.position)
+        {
             target.position = (target.position == posA) ? posB : posA;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("PlayerFeet"))
         {
-            collision.transform.parent = this.transform;
+            collision.transform.parent.parent = this.transform;
         }
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("PlayerFeet"))
         {
-            collision.transform.parent = null;
+            collision.transform.parent.parent = playerOriginalParent;
         }
     }
 }
